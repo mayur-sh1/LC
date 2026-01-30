246c5a9a5ed0f04437e04ebb9a580cd37b6f277c
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-01-31 01:32:13 +0530

    Time: 52 ms (43.18%) | Memory: 53.5 MB (26.82%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
+
         int n = times.length;
 
-        // store arrival, leave, friend index
+        // arrival, leave, original index
         int[][] arr = new int[n][3];
         for (int i = 0; i < n; i++) {
             arr[i][0] = times[i][0];
@@ -12,29 +13,39 @@ class Solution {
             arr[i][2] = i;
         }
 
-        // sort by arrival time
         Arrays.sort(arr, (a, b) -> a[0] - b[0]);
 
-        // chairEnd[i] = when chair i becomes free
-        int[] chairEnd = new int[n];
-        Arrays.fill(chairEnd, -1);
+        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
+
+        // endTime, chairIndex
+        PriorityQueue<int[]> occupiedChairs =
+                new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        // initially all chairs are available
+        for (int i = 0; i < n; i++) {
+            availableChairs.add(i);
+        }
 
         for (int i = 0; i < n; i++) {
             int arrival = arr[i][0];
             int leave = arr[i][1];
             int friend = arr[i][2];
 
-            // find smallest free chair
-            for (int chair = 0; chair < n; chair++) {
-                if (chairEnd[chair] <= arrival) {
-                    chairEnd[chair] = leave;
+            // free chairs whose time is over
+            while (!occupiedChairs.isEmpty() &&
+                   arrival >= occupiedChairs.peek()[0]) {
 
-                    if (friend == targetFriend) {
-                        return chair;
-                    }
-                    break;
-                }
+                int[] chair = occupiedChairs.poll();
+                availableChairs.add(chair[1]);
             }
+
+            int chair = availableChairs.poll();
+
+            if (friend == targetFriend) {
+                return chair;
+            }
+
+            occupiedChairs.add(new int[]{leave, chair});
         }
 
         return -1;
