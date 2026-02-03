52119632a967d8122a33866f1aa3829003c96138
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-02-03 12:33:40 +0530

    Time: 2 ms (99.22%) | Memory: 47.1 MB (98.77%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,18 +1,35 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
-        for(int i=0;i<nums.length;i++){
-            maxHeap.add(nums[i]);
-            minHeap.add(nums[i]);
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            // update max values
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            // update min values
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
         }
-        int max1=maxHeap.poll();
-        int max2=maxHeap.poll();
-        int max3=maxHeap.poll();
 
-        int min1=minHeap.poll();
-        int min2=minHeap.poll();
-
-        return Math.max(max1*max2*max3,max1*min1*min2);
+        return Math.max(max1 * max2 * max3,
+                        min1 * min2 * max1);
     }
 }
