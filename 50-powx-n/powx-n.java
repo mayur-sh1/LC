682ffa8ea8afb87e23d4f6f596862f68a57596e5
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-03-05 18:39:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (79.07%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return solve(x,n); 
+    }
+    public double solve(double x,long n){
+        if(n==0) return 1;
+        if(n<0) return solve(1/x,-n);
+        if(n%2==0) return solve(x*x,n/2); // x^n=(x^2)^n/2
+        else return x*solve(x*x,(n-1)/2);
+    }
+
+}
