65b62a45e78a6beabc812e0ab4bdc0e9616300be
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-03-07 16:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (71.74%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -9,6 +9,7 @@ class Solution {
         return (int) ans;
     }
     public long findPower(long x,long n){
+        x=x%mod;
         if(n==0) return 1;
         if(n<0) return findPower(1/x,-n);
         if(n%2==0) return findPower((x*x)%mod,n/2);
