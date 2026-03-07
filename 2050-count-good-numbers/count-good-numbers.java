f797a891b66b2b417ed0915c6c253f2bc1792bdd
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-03-07 15:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (52.70%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    long mod = 1_000_000_007;
+    public int countGoodNumbers(long n) {
+        long evenIndices = (n + 1) / 2;
+        long oddIndices = n / 2;
+
+        long ans = (findPower(5, evenIndices) * findPower(4, oddIndices)) % mod;
+
+        return (int) ans;
+    }
+    public long findPower(long x,long n){
+        if(n==0) return 1;
+        if(n<0) return findPower(1/x,-n);
+        if(n%2==0) return findPower((x*x)%mod,n/2);
+        else return x*findPower((x*x)%mod,(n-1)/2)%mod;
+    }
+}
