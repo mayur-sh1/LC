0bff85270615a5b8f9ee7ec4a82bb5a807df4565
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-06-13 17:00:08 +0530

    Time: 19 ms (35.28%) | Memory: 50.9 MB (69.83%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,13 +1,40 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        int n=matrix.length;
 
-        for(int [] a:matrix){
-            for(int ele:a) pq.add(ele);
+        // int[] ele,listId,eleId
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+
+        // adding first element of each row 
+       for(int i=0;i<matrix.length;i++){
+            pq.add(new int[]{matrix[i][0],
+            i,0
+            });
+       }
+
+       int counter=1;
+
+       while(!pq.isEmpty()){
+        int arr[]=pq.poll();
+
+        int ele=arr[0];
+        int listId=arr[1];
+        int eleId=arr[2];
+
+        if(counter==k) return ele;
+        
+        counter+=1;
+
+        int nextElementId=eleId+1;
+        if(nextElementId<n) {
+            pq.add(new int[]{
+                matrix[listId][nextElementId],
+                listId,
+                nextElementId
+            });
         }
 
-        while(pq.size()>k) pq.poll();
-
-        return pq.poll();
+       }
+       return -1;
     }
 }
