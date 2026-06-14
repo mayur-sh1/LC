bebc2591007e3d204af5fba31f3c735247d69012
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-06-14 19:42:32 +0530

    Time: 428 ms (25.19%) | Memory: 117.9 MB (22.37%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        List<int[]> list=new ArrayList<>();
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                list.add(new int[]{
+                    arr[i],arr[j]
+                });
+            }
+        }
+        Collections.sort(list,(a,b)->Double.compare(
+            1.0*a[0]/a[1],
+            1.0*b[0]/b[1]
+        ));
+
+        return list.get(k-1);
+    }
+}
