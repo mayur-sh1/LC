a17fe1203453afd00926aed0a336cc23d21a0251
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-07-27 18:14:02 +0530

    Time: 1 ms (84.36%) | Memory: 42.9 MB (86.75%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,32 +1,14 @@
 class Solution {
     public int specialArray(int[] nums) {
-        Arrays.sort(nums);
-
         int n=nums.length;
-        int left=0,right=n-1;   
-        int ans=-1;
-
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int numsFromMid=n-mid;
-
-            if(nums[mid]>=numsFromMid){
-                ans=numsFromMid;
-                right=mid-1;
-            }else{
-                left=mid+1;
-            }
-        }
-
-        //verify
-        int count=0;
-        if(ans!=-1){
+        for(int x=0;x<=n;x++){
+            int count=0;
             for(int num:nums){
-                if(num>=ans) count++;
+                if(num>=x) count++;
             }
+            if(count==x) return x;
         }
-        if(count!=ans) ans=-1;
-
-        return ans;
+        return -1;
     }
+    
 }
