cfc094642f3feea0f0a1889fa0d7c852aaa5fa04
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-08-06 17:29:21 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (50.26%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        for(int i=n;i<=n+9;i++){
+            int prod=1;
+            int temp=i;
+            while(temp!=0){
+                prod=prod*(temp%10);
+                temp/=10;
+            }
+            if(prod%t==0) return i;
+        }
+        return -1;
+    }
+}
