2a812d4cf584c423ff4a690d88a554a7dc010383
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-08-06 20:49:23 +0530

    Time: 27 ms (27.08%) | Memory: 46.6 MB (43.79%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,28 @@
+class Solution {
+    int dp[];
+    public int coinChange(int[] coins, int amount) {
+        dp=new int[amount+1];
+        Arrays.fill(dp,-1);
+
+        int ans=solve(coins,amount);
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+    int solve(int [] coins,int amount){
+        if(amount==0) return 0;
+
+        if(amount<0) return Integer.MAX_VALUE;
+
+        if(dp[amount]!=-1) return dp[amount];
+
+        int min=Integer.MAX_VALUE;
+        for(int coin:coins){
+            int res=solve(coins,amount-coin);
+            
+            if(res!=Integer.MAX_VALUE){
+                min=Math.min(min,1+res);
+            }
+        }
+
+        return dp[amount]=min;
+    }
+}
