0f26710a8ed48cae89f1471602d5929ad146dc7a
mayur-sh1 <167094555+mayur-sh1@users.noreply.github.com>
2026-08-28 15:24:09 +0530

    Time: 32 ms (9.68%) | Memory: 58 MB (5.00%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    Integer[][]dp;
+    public int longestCommonSubsequence(String text1, String text2) {
+        dp=new Integer[text1.length()][text2.length()];
+
+        return solve(0,0,text1,text2);
+    }
+    int solve(int i,int j,String text1,String text2){
+        if(i==text1.length() || j==text2.length()){
+            return 0;
+        }
+
+        if(dp[i][j]!=null) return dp[i][j];
+
+        if(text1.charAt(i)==text2.charAt(j)){
+            return 1+solve(i+1,j+1,text1,text2);
+        }
+
+        return dp[i][j]=Math.max(solve(i+1,j,text1,text2),solve(i,j+1,text1,text2));
+    }
+}
